73149cea1bb458edfd3493fb30a1a065d0049f4b
Chris Dobler <chris.dobler@beautycounter.com>
2018-08-13 16:21:22 -0700

    Adds page 1 view

--- src/Screen1.js
+++ src/Screen1.js
@@ -5,17 +5,8 @@ export class Screen1 extends Component {
   render() {
     return (
       <div className="App">
-        <h1>What active ingredient does Counter Control have?</h1>
-
-        <ul>
-            <li>A) 75% </li>
-            <li>B) 90% </li>
-            <li>C) 95% </li>
-            <li>D) 80%</li>
-        </ul>
-
-        <div> Text your answer to <strong>(805) 874-5677</strong></div>
-
+        <img src="page1.png" style={{width: "70vw"}} />
+        <a>Next</a>
       </div>
     );
   }
