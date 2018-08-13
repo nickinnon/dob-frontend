60bf2a6cf86f44b96fd7df980411003e76bd211c
Chris Dobler <chris.dobler@beautycounter.com>
2018-08-13 16:35:43 -0700

    updates endpoint connection

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -4,16 +4,17 @@ const getEndpoint = (str) => `https://bc-hackathon-dob.herokuapp.com/api/${str}`
 const getJson = (res) => res.json();
 
 const fetchConfig = {
+    credentials: 'omit',
     method: "GET", // *GET, POST, PUT, DELETE, etc.
-    mode: "no-cors", // no-cors, cors, *same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, same-origin, *omit
+    // mode: "no-cors", // no-cors, cors, *same-origin
+    // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    // credentials: "same-origin", // include, same-origin, *omit
     headers: {
        "Content-Type": "application/json",
        // "Content-Type": "application/x-www-form-urlencoded",
     },
-    redirect: "follow", // manual, *follow, error
-    referrer: "no-referrer", // no-referrer, *client
+    // redirect: "follow", // manual, *follow, error
+    // referrer: "no-referrer", // no-referrer, *client
     //body: JSON.stringify(data), // body data type must match "Content-Type" header
 }
 
@@ -26,4 +27,5 @@ export const fetchScores = () => {
 export const fetchOrders = () => {
     fetch(getEndpoint('orders'), fetchConfig)
         .then( getJson )
+        .then((json) => console.log(json)) 
 }
