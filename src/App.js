1df90bd78ffdc93840dea64e3416bc3d5f3f9ba2
nickinnon <nickinnon@gmail.com>
2018-08-13 16:40:29 -0700

    Add switch statement

--- src/App.js
+++ src/App.js
@@ -6,13 +6,31 @@ import { Screen1 } from './Screen1';
 import { Screen2 } from './Screen2';
 import { Screen3 } from './Screen3';
 
+
+
 class App extends Component {
+  state = {
+    i: 0,
+  }
+  pages = [
+    <Screen1 />,
+    <Screen2 />,
+    <Screen3 />,
+  ];
+
+  get showIndex() {
+    return this.state.i % this.pages.length;
+  }
+
+  nextPage = () => {
+    this.setState({i: this.state.i + 1});
+  }
+
   render() {
     return (
       <div className="App">
-        <Screen1 />
-        <Screen2 />
-        <Screen3 />
+        <button onClick={this.nextPage}>Next</button>
+        {this.pages[this.showIndex]}
       </div>
     );
   }
