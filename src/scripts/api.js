12c4cf9fea223d7ea4b48fa8f3317a71411122ac
nickinnon <nickinnon@gmail.com>
2018-08-13 16:21:48 -0700

    Add Fetch logic

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -3,13 +3,27 @@
 const getEndpoint = (str) => `https://bc-hackathon-dob.herokuapp.com/api/${str}`
 const getJson = (res) => res.json();
 
+const fetchConfig = {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "no-cors", // no-cors, cors, *same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, same-origin, *omit
+    headers: {
+       "Content-Type": "application/json",
+       // "Content-Type": "application/x-www-form-urlencoded",
+    },
+    redirect: "follow", // manual, *follow, error
+    referrer: "no-referrer", // no-referrer, *client
+    //body: JSON.stringify(data), // body data type must match "Content-Type" header
+}
+
 export const fetchScores = () => {
-    fetch(getEndpoint('scores'))
+    fetch(getEndpoint('scores'), fetchConfig)
         .then( getJson )
-        .then(console.log('scores')) 
+        .then((json) => console.log(json)) 
 }
 
 export const fetchOrders = () => {
-    fetch(getEndpoint('orders'))
+    fetch(getEndpoint('orders'), fetchConfig)
         .then( getJson )
 }
